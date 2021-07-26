537cd858d3c1d281ca37fcdd8ea6578ff1c57fd5
kbrig <v3lkin@gmail.com>
2021-07-26 23:27:03 +0100

    starting to comment a bit.

--- EncryptionCodingGame/Solver/Core/CoreCaesarSolver.cs
+++ EncryptionCodingGame/Solver/Core/CoreCaesarSolver.cs
@@ -3,15 +3,28 @@ using System.Linq;
 
 namespace EncryptionCodingGame.Solver.Core
 {
+    /// <summary>
+    /// Simple substitution cipher.
+    /// Apply a "shift" for each letters of your message.
+    /// i.e. "HELLO", with a shift of 3, will be "KHOOR"
+    /// </summary>
     public class CoreCaesarSolver : BaseCoreSolver<ICaesarSolver, int>, ICaesarSolver
     {
         private char[] alphabet = Enumerable.Range('A', 26).Select(c => Convert.ToChar(c)).ToArray();
 
+        /// <summary>
+        /// Takes in an encrypted string and returns its plain text.
+        /// </summary>
+        /// <param name="ciphertext">Caesar encrypted input</param>
+        /// <param name="shift">Shift applied to generate said input</param>
+        /// <returns>Plain text</returns>
         public string Decrypt(string ciphertext, int shift)
         {
             string plaintext = "";
             foreach (var character in ciphertext)
             {
+                // Adding this neat little thing to cater for punctuation.
+                // Therefore we only apply substitution on handled characters.
                 if (!alphabet.Contains(character))
                 {
                     plaintext += character;
@@ -31,6 +44,13 @@ namespace EncryptionCodingGame.Solver.Core
             return plaintext;
         }
 
+        /// <summary>
+        /// Encrypts plaintext with a Caesar Cipher.
+        /// Applies a shift to the letter of the text and return the result.
+        /// </summary>
+        /// <param name="plaintext">Text to encrypt</param>
+        /// <param name="shift">Shift to apply</param>
+        /// <returns>Encrypted string</returns>
         public string Encrypt(string plaintext, int shift)
         {
             plaintext = plaintext.ToUpper();
